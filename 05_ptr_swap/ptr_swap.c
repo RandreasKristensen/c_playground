8366ee95fd57c159b6fc8d11f1529961202ed3c0
root <arandkristensen@gmail.com>
2026-05-15 17:09:22 +0000

    setup 05

---
+++ 05_ptr_swap/ptr_swap.c
@@ -0,0 +1,7 @@
+/*
+Write a `swap(int *a, int *b)` function 
+and call it. Then do the same for strings 
+(char pointers).
+Goal: actually feel the difference 
+between pass-by-value and pass-by-pointer.
+*/
